703e3e236ffce49799464f89ba46c953320dfcc7
sikindra97 <sikindramahto374@gmail.com>
2026-03-08 13:56:42 +0530

    Update axios.js

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+ baseURL: "https://testcraft-ze25.onrender.com/api",
 });
 
 api.interceptors.request.use((config) => {
