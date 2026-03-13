72c200a5fab5dbdcc9c36e8e0dbc3c1817c60ff7
sikindra97 <sikindramahto374@gmail.com>
2026-03-13 19:35:21 +0530

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -4,10 +4,17 @@ const userSchema = new mongoose.Schema({
   name: String,
   email: String,
   password: String,
+
   role: {
     type: String,
-    enum: ["admin", "teacher", "student"], // ✅ FIX
+    enum: ["admin", "teacher", "student"],
     default: "student",
+  },
+
+  status: {
+    type: String,
+    enum: ["pending", "approved", "rejected"],
+    default: "pending",
   },
 });
 
