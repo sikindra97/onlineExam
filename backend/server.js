f507563eb6544197c96d145f3da8a2d2a32fcfad
sikindra97 <sikindramahto374@gmail.com>
2026-03-13 19:42:14 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -38,7 +38,7 @@ mongoose
     });
 
     /* SERVER */
-    const PORT = process.env.PORT;
+    const PORT = process.env.PORT || 10000;
 
     app.listen(PORT, () => {
       console.log(`🚀 Server running on port ${PORT}`);
