895e9bc180ae9da470f8f46f0f0887e4d7d05e46
sikindra97 <sikindramahto374@gmail.com>
2026-03-13 19:34:51 +0530

    Update roleMiddleware.js

--- backend/middleware/roleMiddleware.js
+++ backend/middleware/roleMiddleware.js
@@ -1,15 +1,3 @@
-// module.exports = (...roles) => {
-//   return (req, res, next) => {
-//     if (!roles.includes(req.user.role)) {
-//       return res.status(403).json({ message: "Access denied" });
-//     }
-//     next();
-//   };
-// };
-
-
-
-
 
 module.exports = (...allowedRoles) => {
   return (req, res, next) => {
